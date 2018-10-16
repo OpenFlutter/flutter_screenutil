0787b272c68d32c18d6365b60ecbb3c5eda10ce2
李卓原 <zhuoyuan93@gmail.com>
2018-10-16 16:23:32 +0800

    0.3.0文档

--- example/lib/main_zh.dart
+++ example/lib/main_zh.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter_ScreenUtil',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'FlutterScreenUtil Demo'),
     );
   }
 }
