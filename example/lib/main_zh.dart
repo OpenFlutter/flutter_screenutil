db8c25aee52ace9b40bc3be21353593875d97d2e
李卓原 <zhuoyuan93@gmail.com>
2018-10-17 15:32:52 +0800

    优化example

--- example/lib/main_zh.dart
+++ example/lib/main_zh.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter_ScreenUtil',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
