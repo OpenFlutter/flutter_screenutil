8e405a1a0c495d2e2dda86f179ca033e96830273
lizhuoyuan <zhuoyuan93@gmail.com>
2019-08-23 16:26:39 +0800

    0.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,45 +1,80 @@
-## [0.0.1] - first version
+<!--
+ * @Author: zhuoyuan93@gmail.com
+ * @Date: 2018-10-16 19:43:03
+ * @LastEditors: zhuoyuan93@gmail.com
+ * @LastEditTime: 2019-08-23 16:26:24
+ * @Description: Update log
+ -->
 
-## [0.2.2] - Fixed bug when releasing
+# 0.0.1
 
-## [0.2.2] - Optimize documentation
+- first version
 
-## [0.3.0] - Add font size adaptation
+# 0.0.2
 
-## [0.3.0] - Perfect documentation
-Width is enlarged relative to the design draft => The ratio of font and width to the size of the design
-Height is enlarged relative to the design draft => The ratio of  height width to the size of the design
+- Fixed bug when releasing
 
-## [0.4.0] - Optimize font adaptation method
+# 0.2.2
 
-## [0.4.1] - Fix font adaptation issues
+- Optimize documentation
 
-## [0.4.2] - add two Properties
+# 0.3.0
 
-  ///Current device width dp <br/>
-  ///当前设备宽度 dp <br/>
-  ScreenUtil.screenWidthDp 
+- Add font size adaptation
 
-  ///Current device height dp <br/>
-  ///当前设备高度 dp <br/>
-  ScreenUtil.screenHeightDp   
+# 0.3.1
 
-## [0.4.2] - Modify the font to change with the system zoom mode. The default value is false.
+- Perfect documentation
+- Width is enlarged relative to the design draft => The ratio of font and width to the size of the design
+- Height is enlarged relative to the design draft => The ratio of height width to the size of the design
 
-setSp(int fontSize, [allowFontScaling = false]) => allowFontScaling
-      ? setWidth(fontSize) * _textScaleFactor
-      : setWidth(fontSize);
-      
-## [0.4.4] - Fix bugs that default fonts change with the system
+# 0.4.0
 
-## [0.5.1] - Fix the wrong way of using 
+- Optimize font adaptation method
 
-It is recommended to  use `ScreenUtil.getInstance()` instead of `ScreenUtil()` , for example: `ScreenUtil.getInstance().setHeight(25)` instead of `ScreenUtil().setHeight(25)`
+# 0.4.1
 
-## [0.5.2] - Change the parameter type from int to double
+- Fix font adaptation issues
 
-setWidth,setHeight,setSp.
-for example:
-you can use setWidth(100) or setWidth(100.0) 
+# 0.4.2
 
-## [0.5.3] - Change the units of statusBarHeight and bottomBarHeight to dp
+- add two Properties
+- ///Current device width dp
+- ///当前设备宽度 dp
+- ScreenUtil.screenWidthDp
+
+- ///Current device height dp
+- ///当前设备高度 dp
+- ScreenUtil.screenHeightDp
+
+# 0.4.3
+
+- Modify the font to change with the system zoom mode. The default value is false.
+
+- setSp(int fontSize, [allowFontScaling = false]) => allowFontScaling
+? setWidth(fontSize) \* \_textScaleFactor
+: setWidth(fontSize);
+
+# 0.4.4
+
+- Fix bugs that default fonts change with the system
+
+# 0.5.1
+
+- Fix the wrong way of using
+
+- It is recommended to use `ScreenUtil.getInstance()` instead of `ScreenUtil()` , for example: `ScreenUtil.getInstance().setHeight(25)` instead of `ScreenUtil().setHeight(25)`
+
+# 0.5.2
+
+- Change the parameter type from int to double
+
+- setWidth,setHeight,setSp. for example: you can use setWidth(100) or setWidth(100.0)
+
+# 0.5.3
+
+- Change the units of statusBarHeight and bottomBarHeight to dp
+
+# 0.6.0
+
+- Completing comments , adding English commentsWelcome to add, correct
--- lib/flutter_screenutil.dart
+++ lib/flutter_screenutil.dart
@@ -8,9 +8,13 @@ import 'package:flutter/material.dart';
 class ScreenUtil {
   static ScreenUtil instance = new ScreenUtil();
 
-  //设计稿的设备尺寸修改
+  /// UI设计中手机尺寸 , px
+  /// Size of the phone in UI Design , px
   double width;
   double height;
+
+  /// 控制字体是否要根据系统的“字体大小”辅助选项来进行缩放。默认值为false。
+  /// allowFontScaling Specifies whether fonts should scale to respect Text Size accessibility settings. The default is false.
   bool allowFontScaling;
 
   static MediaQueryData _mediaQueryData;
@@ -46,49 +50,66 @@ class ScreenUtil {
 
   static MediaQueryData get mediaQueryData => _mediaQueryData;
 
-  ///每个逻辑像素的字体像素数，字体的缩放比例
+  /// 每个逻辑像素的字体像素数，字体的缩放比例
+  /// The number of font pixels for each logical pixel.
   static double get textScaleFactory => _textScaleFactor;
 
-  ///设备的像素密度
+  /// 设备的像素密度
+  /// The size of the media in logical pixels (e.g, the size of the screen).
   static double get pixelRatio => _pixelRatio;
 
-  ///当前设备宽度 dp
+  /// 当前设备宽度 dp
+  /// The horizontal extent of this size.
   static double get screenWidthDp => _screenWidth;
 
   ///当前设备高度 dp
+  ///The vertical extent of this size. dp
   static double get screenHeightDp => _screenHeight;
 
-  ///当前设备宽度 px
+  /// 当前设备宽度 px
+  /// The vertical extent of this size. px
   static double get screenWidth => _screenWidth * _pixelRatio;
 
-  ///当前设备高度 px
+  /// 当前设备高度 px
+  /// The vertical extent of this size. px
   static double get screenHeight => _screenHeight * _pixelRatio;
 
-  ///状态栏高度 dp 刘海屏会更高
+  /// 状态栏高度 dp 刘海屏会更高
+  /// The offset from the top
   static double get statusBarHeight => _statusBarHeight;
 
-  ///底部安全区距离 dp
+  /// 底部安全区距离 dp
+  /// The offset from the bottom.
   static double get bottomBarHeight => _bottomBarHeight;
 
-  ///实际的dp与设计稿px的比例
+  /// 实际的dp与UI设计px的比例
+  /// The ratio of the actual dp to the design draft px
   get scaleWidth => _screenWidth / instance.width;
 
   get scaleHeight => _screenHeight / instance.height;
 
-  ///根据设计稿的设备宽度适配
-  ///高度也根据这个来做适配可以保证不变形
+  /// 根据UI设计的设备宽度适配
+  /// 高度也可以根据这个来做适配可以保证不变形,比如你先要一个正方形的时候.
+  /// Adapted to the device width of the UI Design.
+  /// Height can also be adapted according to this to ensure no deformation ,
+  /// if you want a square
   setWidth(double width) => width * scaleWidth;
 
-  /// 根据设计稿的设备高度适配
-  /// 当发现设计稿中的一屏显示的与当前样式效果不符合时,
-  /// 或者形状有差异时,高度适配建议使用此方法
-  /// 高度适配主要针对想根据设计稿的一屏展示一样的效果
+  /// 根据UI设计的设备高度适配
+  /// 当发现UI设计中的一屏显示的与当前样式效果不符合时,
+  /// 或者形状有差异时,建议使用此方法实现高度适配.
+  /// 高度适配主要针对想根据UI设计的一屏展示一样的效果
+  /// Highly adaptable to the device according to UI Design
+  /// It is recommended to use this method to achieve a high degree of adaptation
+  /// when it is found that one screen in the UI design
+  /// does not match the current style effect, or if there is a difference in shape.
   setHeight(double height) => height * scaleHeight;
 
   ///字体大小适配方法
-  ///@param fontSize 传入设计稿上字体的px ,
-  ///@param allowFontScaling 控制字体是否要根据系统的“字体大小”辅助选项来进行缩放。默认值为false。
-  ///@param allowFontScaling Specifies whether fonts should scale to respect Text Size accessibility settings. The default is false.
+  ///@param [fontSize] UI设计上字体的大小,单位px.
+  ///Font size adaptation method
+  ///@param [fontSize] The size of the font on the UI design, in px.
+  ///@param [allowFontScaling]
   setSp(double fontSize) => allowFontScaling
       ? setWidth(fontSize)
       : setWidth(fontSize) / _textScaleFactor;
