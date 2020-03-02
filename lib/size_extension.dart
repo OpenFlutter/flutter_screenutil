f3046b351a5e071f087a57d51f8dfaee232e850b
Hosam Hasan <hosamkora@gmail.com>
2020-03-02 16:31:30 +0200

    add sizing extension members to num class

---
+++ lib/size_extension.dart
@@ -0,0 +1,11 @@
+import 'flutter_screenutil.dart';
+
+extension SizeExtension on num {
+  num get w => ScreenUtil().setWidth(this);
+
+  num get h => ScreenUtil().setHeight(this);
+
+  num get sp => ScreenUtil().setSp(this);
+
+  num get ssp => ScreenUtil().setSp(this, allowFontScalingSelf: true);
+}
