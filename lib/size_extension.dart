483f4ea952123addbd77eca997c3cec988f5cd6f
Alex <github@alexv525.com>
2021-03-28 23:19:59 +0800

    Support nnbd

--- lib/size_extension.dart
+++ lib/size_extension.dart
@@ -1,26 +1,26 @@
 import 'package:flutter_screenutil/screenutil.dart';
 
 extension SizeExtension on num {
-  ///[ScreenUtil.setWidth]
+  /// [ScreenUtil.setWidth]
   num get w => ScreenUtil().setWidth(this);
 
-  ///[ScreenUtil.setHeight]
+  /// [ScreenUtil.setHeight]
   num get h => ScreenUtil().setHeight(this);
 
-  ///[ScreenUtil.setSp]
+  /// [ScreenUtil.setSp]
   num get sp => ScreenUtil().setSp(this);
 
-  ///[ScreenUtil.setSp]
+  /// [ScreenUtil.setSp]
   num get ssp => ScreenUtil().setSp(this, allowFontScalingSelf: true);
 
-  ///[ScreenUtil.setSp]
+  /// [ScreenUtil.setSp]
   num get nsp => ScreenUtil().setSp(this, allowFontScalingSelf: false);
 
-  ///屏幕宽度的倍数
-  ///Multiple of screen width
+  /// 屏幕宽度的倍数
+  /// Multiple of screen width
   num get wp => ScreenUtil.screenWidth * this;
 
-  ///屏幕高度的倍数
-  ///Multiple of screen height
+  /// 屏幕高度的倍数
+  /// Multiple of screen height
   num get hp => ScreenUtil.screenHeight * this;
 }
