2b37e6b229d888205e6a13516b8babdc5bd05a31
Mounir Bouaiche <bouaiche.9@gmail.com>
2022-03-28 21:45:24 +0000

    Update example app

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example/main.dart';
+import 'package:example/src/first_method.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
