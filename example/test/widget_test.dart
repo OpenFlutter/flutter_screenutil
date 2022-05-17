812eb48476c6166fad68e4d80fd99ad2d311fab6
Mounir-Bouaiche <bouaiche.9@gmail.com>
2022-05-17 16:03:13 +0100

    Example test widget

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -6,25 +6,79 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example/src/first_method.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('Test Responsiveness', (WidgetTester tester) async {
+    // Declare Sizes
+    const Size designSize = Size(360, 640);
+    const Size initialSize = designSize;
+    const Size biggerSize = Size(480, 920);
+    const Size smallerSize = Size(300, 560);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // We'll use MediaQuery to simulate diffrent screen sizes
+    MediaQueryData currentData = MediaQueryData(size: initialSize);
+    const MediaQueryData biggerData = MediaQueryData(size: biggerSize);
+    const MediaQueryData smallerData = MediaQueryData(size: smallerSize);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Used to find a widget. See [CommonFinders.byKey].
+    final _key = UniqueKey();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Click on button. See code bellow.
+    Future<int> tap() async {
+      await tester.tap(find.byKey(_key));
+      return tester.pumpAndSettle();
+    }
+
+    void testSize(Size size) {
+      expect(1.w, equals(size.width / designSize.width));
+      expect(1.h, equals(size.height / designSize.height));
+      print('[OK] Size: $size, width: ${1.w}, height: ${1.h}');
+    }
+
+    await tester.pumpWidget(StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        return MediaQuery(
+          data: currentData,
+          child: ScreenUtilInit(
+            useInheritedMediaQuery: true,
+            designSize: designSize,
+            builder: (context, child) => MaterialApp(
+              home: Material(
+                child: TextButton(
+                  key: _key,
+                  child: Text('Change data'),
+                  onPressed: () {
+                    setState(() {
+                      currentData = currentData.size == initialSize
+                          // First test with bigger screen
+                          ? biggerData
+                          // Test with smaller screen
+                          : smallerData;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    ));
+
+    // Tests with initial screen size
+    testSize(initialSize);
+
+    // Click On button to simulate changing screen size
+    await tap();
+    // Tests with bigger screen size
+    testSize(biggerSize);
+
+    // Click On button to simulate changing screen size
+    await tap();
+    // Tests with bigger screen size
+    testSize(smallerSize);
+
+    await tester.pumpAndSettle();
   });
 }
